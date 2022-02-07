814d64b015138cfa907b2be1fe0260ad911e9717
JialeChen5012 <2631654c@student.gla.ac.uk>
2022-02-07 17:29:33 +0000

    chapter 4 exercise

--- rango/views.py
+++ rango/views.py
@@ -16,4 +16,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("<a href='/rango/'>Index</a>Rango says here is the about page.")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Jiale'}
+    return render(request, 'rango/about.html', context = context_dict)
