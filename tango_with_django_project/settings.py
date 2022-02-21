1c3072e9af7d84f396a9489fb6a3fad2d289dbb0
JialeChen5012 <2631654c@student.gla.ac.uk>
2022-02-21 01:14:15 +0000

    Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+LOGIN_URL = 'rango:login'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
